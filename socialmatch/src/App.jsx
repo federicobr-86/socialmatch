f638c51c6adc442fc671a81fb3295706f643b6fb
Federico Barbagallo <socializar.actividades@gmail.com>
2025-02-26 10:19:45 -0300

    Folder and files structure

--- socialmatch/src/App.jsx
+++ socialmatch/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Matching from "./pages/Matching";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/matching" element={<Matching />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
