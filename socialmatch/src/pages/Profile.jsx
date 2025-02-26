1743b35a1dd9ee9683f57624a351039b0f2a4e54
Federico Barbagallo <socializar.actividades@gmail.com>
2025-02-26 15:26:17 -0300

    Profile styles added

--- socialmatch/src/pages/Profile.jsx
+++ socialmatch/src/pages/Profile.jsx
@@ -4,16 +4,19 @@ export default function Profile() {
   const { userId } = useParams();
 
   return (
-    <div>
-      <h1>Perfil de Usuario {userId}</h1>
-      <img
-        src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Perfil"
-      />
-      <p>Nombre</p>
-      <p>Edad</p>
-      <p>Descripción</p>
-      <button>Editar</button>
+    <div className="main">
+      <h1>Tu perfil {userId}</h1>
+      <div className="card">
+        <img
+          src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Perfil"
+          className="img"
+        />
+        <p>Nombre</p>
+        <p>Edad</p>
+        <p>Descripción</p>
+        <button className="button">Editar</button>
+      </div>
     </div>
   );
 }
